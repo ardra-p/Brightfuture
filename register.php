c9be6ebfd41947a8f315ae0ad3deb317850e8783
Ardra p <165270747+ardra-p@users.noreply.github.com>
2025-01-26 09:02:23 +0530

    Add files via upload

---
+++ register.php
@@ -0,0 +1,11 @@
+<?php
+
+$username = $_POST["username"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+$confirm_password = $_POST["confirm-password"];
+$dob = $_POST["dob"];
+$gender = $_POST["gender"];
+$phone = $_POST["phone"];
+
+var_dump($username, $email, $password, $confirm_password, $dob, $gender, $phone);
